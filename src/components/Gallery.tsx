56289a6bb66f15b7782a95674af1e200880d8257
Apollonamarsie <mat.wozniak2001@wp.pl>
2024-05-24 00:09:58 +0200

    create gallery

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,8 +1,34 @@
+import { Image } from 'antd';
+
+const pictures = [
+  "src/assets/galeria/c1.jpg",
+  "src/assets/galeria/c2.jpg",
+  "src/assets/galeria/c3.jpg",
+  "src/assets/galeria/c4.jpg",
+  "src/assets/galeria/c5.jpg",
+  "src/assets/galeria/c6.jpg",
+  "src/assets/galeria/c7.jpg",
+  "src/assets/galeria/c8.jpg",
+  "src/assets/galeria/c9.jpg",
+  "src/assets/galeria/c10.jpg",
+  "src/assets/galeria/c11.jpg",
+  "src/assets/galeria/c12.jpg",
+  "src/assets/galeria/c13.jpg",
+  "src/assets/galeria/c14.jpg",
+  "src/assets/galeria/c15.jpg",
+  "src/assets/galeria/c16.jpg",
+  "src/assets/galeria/c17.jpg",
+  "src/assets/galeria/c18.jpg",
+]
 
 const Gallery = () => {
-  return (
-    <div>Gallery</div>
-  )
+  return pictures.map((name) => (
+    <Image
+      width={200}
+      src={name}
+      id={name}
+    />
+  ));
 }
 
 export default Gallery
