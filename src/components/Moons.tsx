83d6a38ca9e6ed1bc96b0379eaffa6758a8efe7f
Apollonamarsie <mat.wozniak2001@wp.pl>
2024-05-23 23:52:19 +0200

    add moons

--- src/components/Moons.tsx
+++ src/components/Moons.tsx
@@ -1,7 +1,67 @@
+import Table from 'react-bootstrap/Table';
 
 const Moons = () => {
   return (
-    <div>Moons</div>
+    <div>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Planeta</th>
+            <th>Największy księzyc</th>
+            <th>Średnica [km]</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Merkury</td>
+            <td colSpan={2}>--Brak księżyca--</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Wenus</td>
+            <td colSpan={2}>--Brak księżyca--</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td>Ziemia</td>
+            <td>Księżyc</td>
+            <td>3475</td>
+          </tr>
+          <tr>
+            <td>4</td>
+            <td>Mars</td>
+            <td>Fobos</td>
+            <td>22,2</td>
+          </tr>
+          <tr>
+            <td>5</td>
+            <td>Jowisz</td>
+            <td>Ganimedes</td>
+            <td>5262</td>
+          </tr>
+          <tr>
+            <td>6</td>
+            <td>Saturn</td>
+            <td>Tytan</td>
+            <td>5150</td>
+          </tr>
+          <tr>
+            <td>7</td>
+            <td>Uran</td>
+            <td>Tytania</td>
+            <td>1577,8</td>
+          </tr>
+          <tr>
+            <td>8</td>
+            <td>Neptun</td>
+            <td>Tryton</td>
+            <td>2706,8</td>
+          </tr>
+        </tbody>
+      </Table>
+    </div>
   )
 }
 
